fc4face50a5480181451478d43b4e856bf21af59
HamzaArs <hamza1arrastec@gmail.com>
2021-06-16 20:11:34 +0100

    All fragments added
    Added three fragments .. first one for audios ,second for favorites and third for playlists

--- app/src/main/java/com/example/listenupv2/ui/MainActivity.java
+++ app/src/main/java/com/example/listenupv2/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.listenupv2;
+package com.example.listenupv2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.listenupv2.R;
 
 public class MainActivity extends AppCompatActivity {
 
